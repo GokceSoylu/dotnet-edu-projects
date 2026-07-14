f110374edb7b27647015b166e85263632afc60d0
GokceSoylu <gokcesoylu24@gmail.com>
2026-07-14 10:55:47 +0300

    for new example

--- Week1_CSharpFundamentals/Models.cs
+++ Week1_CSharpFundamentals/Models.cs
@@ -1,6 +1,4 @@
+using System;
 namespace Week1_CSharpFundamentals;
 
-public readonly record struct CustomerInfo(int Id, String Name, int Age);
-
-public record Customer(String agent, CustomerInfo info);
 
